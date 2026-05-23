d718ef7fa536d254f6cb1b79c5000ab3175e4eae
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-05-23 20:19:51 +0000

    Math and Numbers done

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,26 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof(balance));
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3));
+
+// **************************MATHS*****************************
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(5,3,1,6));
+console.log(Math.max(6,3,4,7));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
