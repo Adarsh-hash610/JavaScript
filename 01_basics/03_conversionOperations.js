26302efa7a2e56ee3431ba4812f9620c78d5e6d9
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-05-23 08:42:54 +0000

    conversion done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,9 +1,9 @@
 let score = "33abx"
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // Conversions...
 /*
@@ -16,15 +16,56 @@ undefined => NaN
 
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false; "abc" => true
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************************* OPERATIONS *********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Namaste"
+let str2 = " Adarsh"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 - "2");
+// console.log("1"- 2);
+// console.log("2" - "2");
+
+
+console.log(+true);     //o/p :- 1
+console.log(+"");       //o/p :- 0
+
+/* Postfix and prefix */
+let x = 3;
+let y = x++
+console.log(`x: ${x} and y: ${y}`);
+y = x;
+console.log(y);
+
+
+let a = 5;
+let b = ++a;
+console.log(`a: ${a} and b: ${b}`);
+
+
+
 
 
 
