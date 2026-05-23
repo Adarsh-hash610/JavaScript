cb5624b43d073f8b9ea05e5a602c6b7d9a074184
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-05-23 15:24:05 +0000

    Memory done

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -12,3 +12,27 @@ console.log(null >= 0);
 console.log("2"===2);   //o/p: false
 
 
+// ********************************************************
+// Stack:- Used in case of primitive type
+// Heap:- Used in case of non-primitive type
+
+let myName = "Adarsh"
+let anotherName = myName
+anotherName = "Babuu"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne ={
+    email:"user@google.com",
+    upi:"user@ybl",
+}
+let userTwo = userOne
+userTwo.email = "adarsh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
