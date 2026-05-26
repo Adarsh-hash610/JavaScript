fd4429855fccb9958d753997eff7f91342fecb5a
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-05-26 12:01:16 +0000

    Scope Part1

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,15 @@
+var c = 300;
+let a = 300
+
+if(true){
+    let a = 10;
+    const b = 20;
+    console.log("Inner: ",a);
+    
+}
+console.log("Outer: ",a);
+// console.log(b);
+console.log(c);
+
+
+
