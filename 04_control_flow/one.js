0b8b58c8dd45613242194854f64e256afc97ba3d
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-01 18:22:18 +0000

    if-else done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+// if
+
+const temperature = 51;
+if(temperature > 50){
+    console.log('Weather is warm');
+    
+}
+console.log('weather is cold');
+
+const score = 200;
+
+
+// if-else
+
+const temp = 50;
+if(temp == 50){
+    console.log('weather is warm');
+    
+}
+else{
+    console.log('weather is cold');
+    
+}
