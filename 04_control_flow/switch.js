7992a2c60c324f89191bac376313c8fdc4f17d10
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-01 18:22:36 +0000

    switch done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,36 @@
+// switch case...
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 6;
+switch (month) {
+    case 1:
+        console.log("January");        
+        break;
+    case 2:
+        console.log("February");        
+        break;
+    case 3:
+        console.log("March");        
+        break;
+    case 6:
+        console.log("June");        
+        break;
+    case 7:
+        console.log("July");        
+        break;
+    case 8:
+        console.log("August");        
+        break;
+
+    default:
+        console.log("default month match...");
+        break;
+}
