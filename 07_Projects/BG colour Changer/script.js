d68969f3bd6b45e4a794568e97777797d29dbb1e
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-08 11:16:42 +0000

    BG colour Changer

---
+++ 07_Projects/BG colour Changer/script.js
@@ -0,0 +1,28 @@
+const randomColour = function(){
+    const hex = '0123456789ABCDEF';
+    let colour = '#';
+    for(let i = 0;i<6;i++)
+    {
+        colour+= hex[Math.floor(Math.random()*16)]
+    }
+    return colour;
+};
+
+let intervalId;
+const startChangingColour = function(){
+    if (!intervalId) {
+        intervalId = setInterval(function(){
+            document.body.style.backgroundColor = randomColour();
+        }, 1000);
+    }
+};
+
+const stopChangingColour = function(){
+    clearInterval(intervalId);
+    intervalId = null
+
+};
+
+document.querySelector('#start').addEventListener('click',startChangingColour);
+
+document.querySelector('#stop').addEventListener('click',stopChangingColour);
